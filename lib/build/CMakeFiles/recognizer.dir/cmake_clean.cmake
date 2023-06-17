a86d5e90ddab8f515e3b7ad050052d271cc43b4a
Diachenko Andrey <adiachenko@astralinux.ru>
2023-06-17 10:02:57 +0300

    remove unnecessary build

--- lib/build/CMakeFiles/recognizer.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "librecognizer.pdb"
-  "librecognizer.so"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/recognizer.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
